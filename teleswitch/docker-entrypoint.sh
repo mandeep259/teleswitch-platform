2dfd02e988d7db203d4284d2569db90e24ab379d
mandeep259 <mandeep259@gmail.com>
2026-02-10 18:14:51 +0530

    CI- File updated

--- teleswitch/docker-entrypoint.sh
+++ teleswitch/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-set -e
 
 echo "=== Starting Teleswitch (FreeSWITCH core) ==="
 echo "TELE_HOME=/home/teleswitch/teleswitch"
+
+# We do not use 'set -e' here so that if the 'nice' level
+# fails (common in CI/locked-down environments), the container doesn't crash.
+# FreeSWITCH will still attempt to run with standard priority.
 
 exec /home/teleswitch/teleswitch/usr/bin/freeswitch \
   -nf \
