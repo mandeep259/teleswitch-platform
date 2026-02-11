f03cecc54e766045ed52f2610a828b7d8314800b
mandeep259 <mandeep259@gmail.com>
2026-02-11 16:21:48 +0530

    indedent fixed in CI file

--- teleswitch/healthcheck.sh
+++ teleswitch/healthcheck.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-fs_cli -x "status" >/dev/null 2>&1
+#fs_cli -x "status" >/dev/null 2>&1
+
+# Use the full path to fs_cli
+/home/teleswitch/teleswitch/usr/bin/fs_cli -x "status" >/dev/null 2>&1
