b7de85fb55610cfcd380a2e26b3d3b4a8ff6adeb
mandeep259 <mandeep259@gmail.com>
2026-02-13 14:57:09 +0530

    rtpengine conf file added with the health checka and defined the env variables.

--- rtpengine/scripts/entrypoint.sh
+++ rtpengine/scripts/entrypoint.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "=== Starting RTPengine ==="
-echo "Config: /etc/rtpengine/rtpengine.conf"
+echo "=== Preparing RTPengine Configuration ==="
 
+# Replace the variable string $EXTERNAL_SIP_IP with the actual env value in the config file
+# We use a temporary file to avoid stream issues
+if [ -n "$EXTERNAL_SIP_IP" ]; then
+    echo "Setting External IP to: $EXTERNAL_SIP_IP"
+    sed -i "s/\$EXTERNAL_SIP_IP/$EXTERNAL_SIP_IP/g" /etc/rtpengine/rtpengine.conf
+else
+    echo "WARNING: EXTERNAL_SIP_IP not set, defaulting to 127.0.0.1"
+    sed -i "s/\$EXTERNAL_SIP_IP/127.0.0.1/g" /etc/rtpengine/rtpengine.conf
+fi
+
+echo "=== Starting RTPengine ==="
+# -f keeps it in the foreground so Docker can manage the process
 exec rtpengine -f --config-file=/etc/rtpengine/rtpengine.conf
